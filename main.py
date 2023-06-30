8c34e3575bcb8b9c3d1edb7bb88654fe1421d50d
alexs <alexs@DESKTOP-MSETNCR>
2023-06-30 09:12:04 -0400

    added new logic with some improvements

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-import random
+import secrets
 import string
+
+uppercase_letters = string.ascii_uppercase
+lowercase_letters = string.ascii_lowercase
+numbers = string.digits
+special_characters = string.punctuation
+
+password = []
+
+password.append(secrets.choice(uppercase_letters))
+password.append(secrets.choice(lowercase_letters))
+password.append(secrets.choice(numbers))
+password.append(secrets.choice(special_characters))
 
 length = int(input('\nEnter the length of password: '))
 
-all = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
-password = "".join(random.sample(all, length))
-print(password)
+character_set = uppercase_letters + lowercase_letters + numbers + special_characters
+for _ in range(length - 4):
+    password.append(secrets.choice(character_set))
+
+secrets.SystemRandom().shuffle(password)
+
+generated_password = ''.join(password)
+
+print("Your password is ", generated_password)
